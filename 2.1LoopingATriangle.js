8cadf709cf21e59a5c4625013a25c67f9f9bff57
Márcia <dg.marciamota@gmail.com>
2019-04-10 22:40:12 +0100

    Create 2.1LoopingATriangle.js

---
+++ 2.1LoopingATriangle.js
@@ -0,0 +1,11 @@
+for (let line='#'; line.length<8;line+='#')
+console.log(line);
+
+//---------------------------------------------
+
+let tag ='#';
+console.log(tag);
+for (i=1; i<=7; i++){
+    tag +='#';
+    console.log(tag);
+}
