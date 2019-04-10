2e0d349418ed17bf8b7ba60c48821518bffa5a46
Márcia <dg.marciamota@gmail.com>
2019-04-10 23:01:36 +0100

    Create 2.2FizzBuzz.js

---
+++ 2.2FizzBuzz.js
@@ -0,0 +1,13 @@
+for (let i=1; i<=100; i++){
+    let five = i%5===0;
+    let three = i%3===0;
+if (three && five) {
+    console.log('fizzbuzz');
+} else if (three) {
+    console.log('fizz');
+} else if (five) {
+    console.log('buzz')
+} else {
+    console.log(i);
+}
+}
